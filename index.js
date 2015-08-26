cb899e059eaf18eb68759ce2910fd162077f07a1
Peter Bodechtel <sweetlikepete@gmail.com>
2015-08-26 13:21:53 +0200

    stub bower

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-/**
- * Escape special characters in the given string of html.
- *
- * @param  {String} value
- * @return {String}
- */
 module.exports = {
 
+   /**
+    * Escape special characters in the given string of html.
+    *
+    * @param  {String} value
+    * @return {String}
+    */
     wtf : function(value) {
 
         return "wtf:" + value;
 
-    }
+    },
+
+    bower : require('tasks/bower')
 
 }
---
+++ tasks/bower.js
@@ -0,0 +1,15 @@
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} value
+ * @return {String}
+ */
+module.exports = {
+
+    install : function(){
+
+        return "bower install";
+
+    }
+
+}
